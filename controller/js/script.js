2b04179ae89324e735922803780d92c304e99be9
unknown <clizan-ws@hotmail.com>
2025-06-16 18:23:01 -0300

    feat: initial menu page

--- controller/js/script.js
+++ controller/js/script.js
@@ -1,12 +1,36 @@
+/*Script responsable index.html page*/
 $(document).ready(function () {
+    $('.btn-singin').click(function () {
+        /*recover the variables*/
+        var getUser = document.getElementById('input-user').value;
+        var getPassword = document.getElementById('input-password').value;
 
-    /*recover the variables*/
-    var user = document.getElementsByName('input-user').innerHTML;
-    var password = document.getElementsByName('input-password').innerHTML;
+        if (getUser && getPassword) {
+           $.ajax({
+                type: 'POST',
+                url: './controller/validation.php',
+                data: {
+                    passUser: getUser,
+                    passPassword: getPassword
+                },
+                success: function(response){
+                    var getResponse = Number(response.trim());
 
-    if(user && password) {
-        $('.btn-singin').click(function () {
-            alert('cliquei');
-        });
-    }
+                    if (getResponse === 1){
+                        window.location.assign("./view/menu.html");
+                    } else{
+                        window.location.assign("index.html");
+                    }
+                }
+
+           })
+        } else {
+            //alert('Campos vazios');
+        }
+    });
 });   
+
+/*Script responsable menu.html*/
+document.getElementById('open_btn').addEventListener('click', function (){
+    document.getElementById('sidebar').classList.toggle('open-sidebar');
+});
